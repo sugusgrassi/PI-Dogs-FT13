37c86a2a666099d0febf4bfd2e66b2a47ad05178
sugus.grassi <sugus.grassi@gmail.com>
2021-06-05 16:03:12 -0300

    agregado de variables y DBMS ER model

---
+++ api/constants.js
@@ -0,0 +1,8 @@
+const BASE_URL = "https://api.thedogapi.com/v1/breeds";
+const CHARACTER_URL = "https://api.thedogapi.com/v1/breeds/search?q={raza_perro}";
+
+
+module.exports = {
+    BASE_URL,
+    CHARACTER_URL
+}
--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -2,9 +2,25 @@ const { DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
-  // defino el modelo
+  // defino dog según mi modelo de Entidad-Relación
   sequelize.define('dog', {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true
+    },
     name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    height: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    weight: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    life_span: {
       type: DataTypes.STRING,
       allowNull: false,
     },
